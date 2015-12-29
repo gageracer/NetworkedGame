617538ba4d53ed1c3c587d3dace3a31d88766c23
gageracer <gageracer@gmail.com>
2015-12-29 17:08:44 +0200

    Labirent düzenlendi.

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -3,12 +3,19 @@ using UnityEngine.Networking;
 
 public class Button : NetworkBehaviour {
 
-
+	private Vector3 from = this.parent.transform.position;
+	[SerializeField]
+	GameObject otherDoor;
 	[SyncVar]
-	private int _cNum = 1;
+	private int _cNum;
 
 	// Use this for initialization
 	void Start () {
+			
+		if (transform.parent.tag == "Pink Door")
+			_cNum = 2;
+		if (transform.parent.tag == "Yellow Door")
+			_cNum = 1;
 
 
 	}
@@ -16,16 +23,26 @@ public class Button : NetworkBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+
 		ColortoNumber ();
 		//ButtonPushed ();
 	
 	}
 
 	public void ButtonPushed(){
-		if(GetComponent<MeshRenderer>().material.color == Color.red)
-		_cNum = 2;
+		if (_cNum == 1) {
+			_cNum =4;
+			if(otherDoor.transform.GetChild(0).GetComponent<MeshRenderer>().material.color == Color.blue)
+			{}
+
+		} else if (_cNum == 2) {
+			_cNum = 4;
+			if(otherDoor.transform.GetChild(0).GetComponent<MeshRenderer>().material.color == Color.blue)
+			{}
+		}
 
 	}
+
 	private void ColortoNumber(){
 		if (_cNum == 1)
 			GetComponent<MeshRenderer>().material.color = Color.red;
@@ -33,6 +50,8 @@ public class Button : NetworkBehaviour {
 			GetComponent<MeshRenderer>().material.color = Color.yellow;
 		else if (_cNum == 3)
 			GetComponent<MeshRenderer>().material.color = Color.green;
+		else if(_cNum == 4)
+			GetComponent<MeshRenderer>().material.color = Color.blue;
 	}
 
 }
